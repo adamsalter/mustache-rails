c4e19cb5ccdbbf365730638449a533147d903a8f
Adam Salter <adam@codebright.net>
2009-10-17 21:01:46 +1100

    - simplify include

--- init.rb
+++
@@ -1,8 +0,0 @@
-require "mustache"
-
-require 'mustache/rails/helper'
-require 'action_view/template_handlers/mustache'
-
-ActionView::Template.register_template_handler(:mustache, ActionView::TemplateHandlers::Mustache)
-
-# ActiveSupport::Dependencies.load_paths << Rails.root.join("app", "views")
--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,41 @@
-Kernel.load File.join(File.dirname(__FILE__), '..', 'init.rb')
+require "mustache"
+
+module ::ActionView
+  module TemplateHandlers
+    class Mustache < TemplateHandler
+      include Compilable
+
+      def compile(template)
+        view_action_helper =  "%sHelper::%s" % [template.base_path.classify.pluralize, template.name.classify]
+        compiled_template = ::Mustache::Template.new(template.source, '.', 'mustache').compile
+
+        template_in_context = <<-EOT
+        extend ::Mustache::Rails::Helper
+        extend #{view_action_helper} if defined? #{view_action_helper}
+        #{compiled_template}
+        EOT
+        
+        template_in_context
+      end
+    end
+  end
+end
+
+class ::Mustache
+  module Rails
+    module Helper
+      def context
+        @context ||= ::Mustache::Context.new(self)
+      end
+      def [](key)
+        context[key.to_sym]
+      end
+      def []=(key, value)
+        context[key.to_sym] = value
+      end
+      alias :ctx :context
+    end
+  end
+end
+
+::ActionView::Template.register_template_handler(:mustache, ::ActionView::TemplateHandlers::Mustache)
