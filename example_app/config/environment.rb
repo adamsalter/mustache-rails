9b064d917dceaf54d43b6f929469c98d208c306b
Adam Salter <adam@codebright.net>
2009-10-17 17:24:01 +1100

    - add in haml for testing

--- example_app/config/environment.rb
+++ example_app/config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rspec", :lib => false
   config.gem "rspec-rails", :lib => false
+  # config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
