ddadaa540b93d9c3087b7ad77e34be36fba75557
Adam Salter <adam@codebright.net>
2009-10-17 18:44:29 +1100

    - update template_handler to include view_action_helpers

--- lib/action_view/template_handlers/mustache.rb
+++ lib/action_view/template_handlers/mustache.rb
@@ -4,7 +4,25 @@ module ActionView
       include Compilable
 
       def compile(template)
-        src = ::Mustache.render("#{template.source}")
+        view_action_helper =  "%sHelper::%s" % [template.base_path.classify.pluralize, template.name.classify]
+        compiled_template = ::Mustache::Template.new(template.source, '.', 'mustache').compile
+
+        template_in_context = <<-EOT
+        extend #{view_action_helper} if defined? #{view_action_helper}
+        def context
+          @context ||= ::Mustache::Context.new(self)
+        end
+        def [](key)
+          context[key.to_sym]
+        end
+        def []=(key, value)
+          context[key.to_sym] = value
+        end
+        ctx = context
+        #{compiled_template}
+        EOT
+        
+        template_in_context
       end
     end
   end
