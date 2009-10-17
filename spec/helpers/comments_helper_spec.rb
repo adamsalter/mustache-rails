7400572114effb28945a59a9c69b39d5aca68155
Adam Salter <adam@codebright.net>
2009-10-17 20:33:49 +1100

    - helpers test

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -5,7 +5,7 @@ describe CommentsHelper do
   it "is included in the helper object" do
     included_modules = (class << helper; self; end).send :included_modules
     included_modules.should include(CommentsHelper)
-    included_modules.should include(PostsHelper)
+    included_modules.should_not include(PostsHelper)
   end
 
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -6,8 +6,8 @@ describe PostsHelper do
   it "is included in the helper object" do
     included_modules = (class << helper; self; end).send :included_modules
     included_modules.should include(PostsHelper)
-    included_modules.should include(CommentsHelper)
-    included_modules.should !include(CommentsHelper::Show)
+    included_modules.should_not include(CommentsHelper)
+    included_modules.should_not include(CommentsHelper::Show)
   end
 
 end
